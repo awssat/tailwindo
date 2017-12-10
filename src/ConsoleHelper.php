bbafb6916fad5b99a56fcab4a69158dfe564073a
Austin Kregel <austin@kbcomp.co>
2017-12-10 23:07:11 +0000

    Add recursive argument to the constructor, and add a way to recursively get all files.

--- src/ConsoleHelper.php
+++ src/ConsoleHelper.php
@@ -8,20 +8,36 @@ class ConsoleHelper
 {
     protected $converter;
     protected $output;
+    protected $recursive = false;
 
-    public function __construct(OutputInterface $output)
+    public function __construct(OutputInterface $output, $recursive)
     {
         $this->converter = new Converter();
         $this->output = $output;
+        $this->recursive = $recursive;
     }
 
     public function folderConvert($folderPath)
     {
         $this->output->writeln('<question>Start Converting Folder: </question>'.$folderPath);
-
-        foreach (new \DirectoryIterator($folderPath) as $file) {
-            if ($file->isFile() && !$file->isDot() && $this->isConvertableFile($file->getExtension())) {
-                $this->fileConvert($file->getRealPath());
+    
+        if ($this->recursive) {
+            $iterator = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator(
+                    $folderPath, \RecursiveDirectoryIterator::SKIP_DOTS
+                ),
+                \RecursiveIteratorIterator::SELF_FIRST,
+                \RecursiveIteratorIterator::CATCH_GET_CHILD
+            );
+        } else {
+            $iterator = new \DirectoryIterator($folderPath);
+        }
+        
+        foreach ($iterator as $path => $directory) {
+            $extensions = explode('.', $directory);
+            $extension = end($extensions);
+            if ($directory->isFile() && $this->isConvertableFile($extension)) {
+                $this->fileConvert($directory->getRealPath());
             }
         }
     }
